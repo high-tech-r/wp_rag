3dd9827f9e341eadf63f3c0784a4650a177f6ba3
Sasaki Kojiro <101150762+high-tech-r@users.noreply.github.com>
2024-12-15 23:00:39 +0900

    Delete wordpress_rag directory

--- wordpress_rag/post_vectors_create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE post_vectors (
-    post_id BIGINT(20) UNSIGNED NOT NULL,
-    vector JSON NOT NULL,
-    PRIMARY KEY (post_id),
-    FOREIGN KEY (post_id) REFERENCES wp_posts(ID) ON DELETE CASCADE
-);
